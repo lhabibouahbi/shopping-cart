aa593248e4ba720f585d5475e468e3a341e5f120
el habib ouahbi <elhabibouahbi@gmail.com>
2021-10-04 17:10:33 +0200

    Add files via upload

---
+++ shopX/webapp/controller/Master.controller.js
@@ -0,0 +1,182 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/m/MessageBox',
+	'sap/f/library',
+	"sap/ui/core/IconPool",
+	"sap/m/Dialog",
+	"sap/m/DialogType",
+	"sap/m/Button",
+	"sap/m/ButtonType",
+	"sap/m/List",
+	"sap/m/StandardListItem",
+	"sap/m/Text",
+	"sap/m/MessageToast",
+	"sap/ui/Device",
+	"sap/base/Log"
+], function (JSONModel, Controller, MessageToast, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary, IconPool, Dialog, DialogType, Button, ButtonType, List, StandardListItem, Text, Device, Log) {
+	"use strict";
+
+
+	return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
+		onInit: function () {
+		//	oSplitContainer.setMode(sap.m.SplitAppMode.HideMode);
+			
+			this.oView = this.getView();
+			this._bDescendingSort = false;
+			this.oProductsTable = this.oView.byId("productsTable");
+			this.oRouter = this.getOwnerComponent().getRouter();
+
+
+
+		},
+
+
+		onEditToggleButtonPress: function() {
+			alert()
+		}, 
+		onExit: function () {
+			Device.orientation.detachHandler(this.onOrientationChange, this);
+		},
+
+		onOrientationChange: function (mParams) {
+			var sMsg = "Orientation now is: " + (mParams.landscape ? "Landscape" : "Portrait");
+			MessageToast.show(sMsg, { duration: 5000 });
+		},
+
+		onAfterRendering: function () {
+			var oSplitContainer = this.getView().byId("SplitAppDemo")
+			oSplitContainer.setMode(sap.m.SplitAppMode.StretchCompressMode);
+			if(oSplitContainer.isMasterShown()){
+		   oSplitContainer.setMode(sap.m.SplitAppMode.HideMode)}
+
+			var oSplitCont = this.getSplitContObj(),
+				ref = oSplitCont.getDomRef() && oSplitCont.getDomRef().parentNode;
+			// set all parent elements to 100% height, this should be done by app developer, but just in case
+			if (ref && !ref._sapUI5HeightFixed) {
+				ref._sapUI5HeightFixed = true;
+				while (ref && ref !== document.documentElement) {
+					var $ref = jQuery(ref);
+					if ($ref.attr("data-sap-ui-root-content")) { // Shell as parent does this already
+						break;
+					}
+					if (!ref.style.height) {
+						ref.style.height = "100%";
+					}
+					ref = ref.parentNode;
+				}
+			}
+		},
+
+
+		onPressNavToDetail: function () {
+			this.getSplitAppObj().to(this.createId("detailDetail"));
+		},
+
+		onPressDetailBack: function () {
+			this.getSplitAppObj().backDetail();
+		},
+
+
+		onListItemPress: function (oEvent) {
+			var sToPageId = oEvent.getParameter("listItem").getCustomData()[0].getValue();
+
+			this.getSplitAppObj().toDetail(this.createId(sToPageId));
+		},
+
+		onPressModeBtn: function (oEvent) {
+			var sSplitAppMode = oEvent.getSource().getSelectedButton().getCustomData()[0].getValue();
+
+			this.getSplitAppObj().setMode(sSplitAppMode);
+			MessageToast.show("Split Container mode is changed to: " + sSplitAppMode, { duration: 5000 });
+		},
+
+
+
+		onDefaultDialogPress: function () {
+			if (!this.oDefaultDialog) {
+				this.oDefaultDialog = new Dialog({
+					title: "Available Products",
+					content: new List({
+						items: {
+							path: "/ProductCollection",
+							template: new StandardListItem({
+								title: "{Name}",
+								counter: "{Quantity}"
+							})
+						}
+					}),
+					beginButton: new Button({
+						type: ButtonType.Emphasized,
+						text: "OK",
+						press: function () {
+							this.oDefaultDialog.close();
+						}.bind(this)
+					}),
+					endButton: new Button({
+						text: "Close",
+						press: function () {
+							this.oDefaultDialog.close();
+						}.bind(this)
+					})
+				});
+
+				// to get access to the controller's model
+				this.getView().addDependent(this.oDefaultDialog);
+			}
+
+			this.oDefaultDialog.open();
+		},
+
+		onSearch: function (oEvent) {
+			var oTableSearchState = [],
+				sQuery = oEvent.getParameter("query");
+
+			if (sQuery && sQuery.length > 0) {
+				oTableSearchState = [new Filter("Name", FilterOperator.Contains, sQuery)];
+			}
+
+			this.oProductsTable.getBinding("items").filter(oTableSearchState, "Application");
+		},
+
+
+		onSort: function () {
+			this._bDescendingSort = !this._bDescendingSort;
+			var oBinding = this.oProductsTable.getBinding("items"),
+				oSorter = new Sorter("Name", this._bDescendingSort);
+
+			oBinding.sort(oSorter);
+		},
+
+		onListItemPress: function (oEvent) {
+			var productPath = oEvent.getSource().getBindingContext("products").getPath(),
+				product = productPath.split("/").slice(-1).pop(),
+				oNextUIState;
+			this.getOwnerComponent().getHelper().then(function (oHelper) {
+				oNextUIState = oHelper.getNextUIState(1);
+				this.oRouter.navTo("detail", {
+					layout: oNextUIState.layout,
+					product: product
+				});
+			}.bind(this));
+		},
+
+		onAddtocart: function(oEvent) {
+
+
+			var oItem = oEvent.getSource().getBindingContext('products').getPath();
+			
+		
+			
+
+			console.log(oItem)
+
+		},
+		
+	});
+
+	
+});
